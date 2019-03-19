6fa316caa0fcc3a51c628e7e5cb92aaee298ea4b
Edward Seley <edwardseley@gmail.com>
2019-03-19 19:49:43 -0400

    Changed Unirest API to Volley API, added Recipe View creater, and added Activities and Layouts

--- app/src/main/java/com/example/thomasstephenson/lazychef/Query.java
+++ app/src/main/java/com/example/thomasstephenson/lazychef/Query.java
@@ -1,10 +1,23 @@
 package com.example.thomasstephenson.lazychef;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.Volley;
+import android.app.Activity;
 import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,8 +31,8 @@ import org.json.JSONObject;
 public class Query {
     private String[] ingNames;
     //Instance of the query constructor for there not being
-    public void QueryIngredients(){
-        QueryIngredientsHelper(this.ingNames);
+    public void QueryIngredients(Activity activity){
+        QueryIngredientsHelper(this.ingNames, activity);
     }
 
 
@@ -28,12 +41,126 @@ public class Query {
      */
     private final String MASHAPE_AUTH = "9c1a1208bbmsh1a3a7f5fe78b0a7p15f439jsn581ffaba03cb";
 
-    //find recipes that match ingredients
-    private List<Recipe> QueryIngredientsHelper(String[] ingredientNames) {
-        List<Recipe> recipes = new ArrayList<>();
-        HttpResponse<JsonNode> response;
-        try {
+    List<Recipe> recipes = new ArrayList<>();
 
+    public void getRecipeDetails(String request, final String recipeName, final Activity activity) {
+        RequestQueue requestQueue = Volley.newRequestQueue(activity);
+        JsonObjectRequest JSONrequest = new JsonObjectRequest(Request.Method.GET, request, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                addRecipe(response, recipeName, activity);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<>();
+                params.put("X-RapidAPI-Key", MASHAPE_AUTH);
+                return params;
+            }
+        };
+
+        requestQueue.add(JSONrequest);
+    }
+
+    public void getRecipes(String request, final Activity activity) {
+        RequestQueue requestQueue = Volley.newRequestQueue(activity);
+        JsonArrayRequest JSONrequest = new JsonArrayRequest(Request.Method.GET, request, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                createRecipes(response, activity);
+            }
+        },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO: Handle error
+                }
+            })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<>();
+                params.put("X-RapidAPI-Key", MASHAPE_AUTH);
+                return params;
+            }
+        };
+
+        requestQueue.add(JSONrequest);
+    }
+
+    public void addRecipe(JSONObject jsonRecipeDetails, String recName, Activity activity) {
+        try {
+            String instr = jsonRecipeDetails.getString("instructions");
+            int preptime = jsonRecipeDetails.getInt("readyInMinutes");
+            int servings = jsonRecipeDetails.getInt("servings");
+
+            List<Ingredient> ingList = new ArrayList<>();
+            JSONArray jsonRecipeIngredients = jsonRecipeDetails.getJSONArray("extendedIngredients");
+
+            //extract ingredients
+            for (int j = 0; j < jsonRecipeIngredients.length(); j++) {
+                //fields needed for Ingredient constuctor
+                String ingName;
+                int ingAmount;
+                String ingUnit;
+                int ingCal;
+
+                //extracting ingredient information
+                JSONObject jsonIngredient = jsonRecipeIngredients.getJSONObject(j);
+                ingName = jsonIngredient.getString("name");
+                ingAmount = jsonIngredient.getInt("amount");
+                ingUnit = jsonIngredient.getString("unit");
+                ingList.add(new Ingredient(ingName, ingAmount, ingUnit));
+            }
+            Recipe recipe = new Recipe(recName, ingList, instr, preptime, servings);
+            recipes.add(recipe);
+            MainActivity.createRecipeView(recipe, activity);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //making an array of recipes out of the JSON blob that the API returned
+    public void createRecipes(JSONArray jsonRecipes, Activity activity) {
+        for (int i = 0; i < jsonRecipes.length(); i++) {
+
+            //fields needed for recipe constructor
+            String recName;
+            List<Ingredient> ingList;
+            String instr;
+            int preptime;
+            int servings;
+            int id;
+            try {
+                JSONObject jsonRecipe = jsonRecipes.getJSONObject(i);
+                Log.d("RECIPE NAME", jsonRecipe.toString());
+                recName = jsonRecipe.getString("title");  //get the name of the recipe
+                id = jsonRecipe.getInt("id"); //get the id of the recipe, from which we will extract the other information
+                String request = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + id + "/information";
+
+                //extract recipe information from API
+                //make into JSON Object
+                getRecipeDetails(request, recName, activity);
+
+                //extract more fields needed for constructor
+            }
+            catch (Exception e) {
+                Log.e("ERROR", e.getMessage());
+            }
+        }
+    }
+    //find recipes that match ingredients
+    public void QueryIngredientsHelper(String[] ingredientNames, Activity activity) {
+        List<Recipe> recipes = new ArrayList<>();
+        try {
             //building the request String that will query the API
             StringBuilder request = new StringBuilder("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=2&ingredients=");
             for (int i = 0; i < ingredientNames.length; i++) {
@@ -42,73 +169,10 @@ public class Query {
                     request.append("%2C");
                 }
             }
-
-            // this is where we access the API in search of recipes that match the ingredients available
-            response = Unirest.get(request.toString())
-                    .header("X-RapidAPI-Key", MASHAPE_AUTH)
-                    .asJson();
-
-            //making an array of recipes out of the JSON blob that the API returned
-            JSONArray jsonRecipes = response.getBody().getArray();
-            for (int i = 0; i < jsonRecipes.length(); i++) {
-
-                //fields needed for recipe constructor
-                String recName;
-                List<Ingredient> ingList;
-                String instr;
-                int preptime;
-                int servings;
-                int id;
-
-                JSONObject jsonRecipe = jsonRecipes.getJSONObject(i);
-                recName = jsonRecipe.getString("title");  //get the name of the recipe
-                id = jsonRecipe.getInt("id"); //get the id of the recipe, from which we will extract the other information
-
-                //extract recipe information from API
-                HttpResponse<JsonNode> recipeResponse = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/" + id + "/information")
-                        .header("X-RapidAPI-Key", MASHAPE_AUTH)
-                        .asJson();
-
-                //make into JSON Object
-                JSONObject jsonRecipeDetails = recipeResponse.getBody().getObject();
-
-                //extract more fields needed for constructor
-                instr = jsonRecipeDetails.getString("instructions");
-                preptime = jsonRecipeDetails.getInt("readyInMinutes");
-                servings = jsonRecipeDetails.getInt("servings");
-
-                ingList = new ArrayList<>();
-                JSONArray jsonRecipeIngredients = jsonRecipeDetails.getJSONArray("extendedIngredients");
-
-                //extract ingredients
-                for (int j = 0; j < jsonRecipeIngredients.length(); j++) {
-                    //fields needed for Ingredient constuctor
-                    String ingName;
-                    int ingAmount;
-                    String ingUnit;
-                    int ingCal;
-
-                    //extracting ingredient information
-                    JSONObject jsonIngredient = jsonRecipeIngredients.getJSONObject(j);
-                    ingName = jsonIngredient.getString("name");
-                    ingAmount = jsonIngredient.getInt("amount");
-                    ingUnit = jsonIngredient.getString("unit");
-                    ingList.add(new Ingredient(ingName, ingAmount, ingUnit));
-                }
-
-                recipes.add(new Recipe(recName, ingList, instr, preptime, servings));
-            }
+            getRecipes(request.toString(), activity);
+        } catch (Exception e) {
+            Log.e("ERROR", e.getMessage());
         }
-
-        catch (UnirestException e) { //error from accessing the API
-            Log.e("Query-API", e.getMessage());
-        }
-
-        catch (JSONException e) { //error parsing JSON
-            Log.e("Query-JSON", e.getMessage());
-        }
-
-        return recipes;
     }
 
     public void AddToQueryIngredients(String ingredientName) {
