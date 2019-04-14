84708ade7102ea68c7ccb481ba49e35391f00151
tps48 <tps48@case.edu>
2019-04-14 18:49:30 -0400

    fix dao id uniqueness

--- app/src/main/java/com/example/thomasstephenson/lazychef/RecipeDao.java
+++ app/src/main/java/com/example/thomasstephenson/lazychef/RecipeDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
@@ -14,7 +15,7 @@ import android.arch.persistence.room.Update;
  */
 @Dao
 public interface RecipeDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(RecipeEntity... recipes);
 
     @Update
